82655cec95352fbdfec835f47bd4e28a4f951c8a
abhinn2@github <abhinnvyas2@gmail.com>
2022-07-26 16:14:28 +0530

    implemented typescript and developed the post section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home: NextPage<Props> = ({ posts }: Props) => {
+  console.log(posts);
   return (
     <div className="max-w-5xl mx-auto">
       <Head>
@@ -12,8 +20,58 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <Banner />
+
+      <div>
+        {posts.map((post) => (
+          <Link key={post._id} href={post.slug.current}>
+            <div>
+              <img
+                className="object-contain w-48"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div>
+                <div>
+                  <p>{post.title}</p>
+                  <p>
+                    {post.description}
+                    <span className="font-bold"> by {post.author.name}</span>
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 object-cover rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type=="post"]{
+    _id,
+    title,
+    slug,
+    mainImage,
+    description,
+    author -> {
+    name,
+    image
+  }
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
--- sanity.js
+++ sanity.js
@@ -1,8 +1,5 @@
-import {
-  createClient,
-  createCurrentUserHook,
-  createImageUrlBuilder,
-} from "next-sanity";
+import { createClient } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
@@ -13,5 +10,3 @@ export const config = {
 
 export const sanityClient = createClient(config);
 export const urlFor = (source) => createImageUrlBuilder(config).image(source);
-
-export const useCurrentUser = createCurrentUserHook(config);
